212ee91e05f40b0b8f999bc4ed6d0ef7bd73bfdf
Jerome <jeromerefuerzodeguzman@yahoo.com>
2013-10-29 13:25:58 +0800

    add a addtional div

--- app/views/messages.blade.php
+++ app/views/messages.blade.php
@@ -3,17 +3,20 @@
 
 @section('content')
 	<div class="row">
-		<div class="small-5 large-centered columns" style="border: 1px solid whitesmoke; background: whitesmoke;">
-			<br />
-			<img src="img/user.jpg" width="50px">&nbsp;&nbsp;&nbsp;<span style="font-weight: bold;">{{ $phonenumber }}</span>
-			<hr />
-			@foreach($lists as $list)
-			<p style="font-size: 14px; clear:both" class="triangle-isosceles <?= $list->to==$phonenumber? "right":"left"?>" >
-				<span style="font-size: 11px; color: gray;">{{ $list->created_at }}:</span>
+		<div class="small-5 large-centered columns">
+			<div class="large-12 columns"  style="border: 1px solid whitesmoke; background: whitesmoke;">
 				<br />
-				<span>{{ $list->message }}</span>
-			</p>
-			@endforeach
+				<img src="img/user.jpg" width="50px" />&nbsp;&nbsp;&nbsp;
+				<span style="font-weight: bold;">{{ $phonenumber }}</span>
+				<hr />
+				@foreach($lists as $list)
+				<p style="display: block; font-size: 14px; clear:both" class="triangle-isosceles <?= $list->to==$phonenumber? "right":"left"?>" >
+					<span style="font-size: 11px; color: gray;">{{ $list->created_at }}:</span>
+					<br />
+					<span>{{ $list->message }}</span>
+				</p>
+				@endforeach
+			</div>
 		</div>
 	</div>
 @endsection
